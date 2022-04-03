46fcb676606a4cb951668be183820afaf1d49f98
Alexander Nagy <nagyal3x@gmail.com>
2022-04-03 18:26:44 +0200

    🍻

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Ali did it!" />
+      <HelloWorld msg="Ali did it! 🍻" />
     </div>
   </header>
 
